0004f26282d95d40688164f9863cc7237532911a
shin-sj <sujung5092@gmail.com>
2022-03-22 00:36:37 +0900

    Add c6066

---
+++ c6066.py
@@ -0,0 +1,13 @@
+a, b, c = map(int, input().split())
+if a % 2 == 0:
+    print("even")
+elif a % 2 != 0 :
+    print("odd")
+if b % 2 == 0:
+        print("even")
+elif b % 2 != 0 :
+    print("odd")
+if c % 2 == 0:
+        print("even")
+elif c % 2 != 0 :
+    print("odd")
