1c89e34973816685d5524a27c4d366a62fa4b8c7
shin-sj <sujung5092@gmail.com>
2022-03-22 00:36:44 +0900

    Add c6067

---
+++ c6067.py
@@ -0,0 +1,11 @@
+a = int(input())
+if a < 0 : 
+    if a % 2 == 0:
+        print("A")
+    elif a % 2 != 0 :
+        print("B")
+else :
+    if a % 2 == 0:
+        print("C")
+    elif a % 2 != 0 :
+        print("D")
